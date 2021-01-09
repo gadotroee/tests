5276079a543911dcd13963496f39cfcf74225c97
Roee Gadot <roee.gadot@up9.com>
2021-01-09 19:26:17 +0200

    no message

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="example-utils",
-    version="1.0.0",
+    version="2.0.0",
     author="",
     author_email="",
     description="A package that makes it easy to create Pypi packages",
